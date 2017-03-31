119257a1abfd1be9c76b07b299b5e363b1db8675
Franck De Goër <degoerdf@ensimag.fr>
2017-03-31 19:16:56 +0200

    Refine models

--- pourparler/website/models.py
+++ pourparler/website/models.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 from django.db import models
@@ -5,14 +7,60 @@ from django.contrib.auth.models import User
 
 from website.random_primary import RandomPrimaryIdModel
 
+
+class Locutor(RandomPrimaryIdModel):
+    user = models.OneToOneField(User, related_name="locutor")
+    friends = models.ManyToManyField("Locutor")
+    ## Préférences de l'utilisateur (todo)
+
+
 class Event(RandomPrimaryIdModel):
-    owner = models.ForeignKey(User)
-    date = models.DateField(default=None, blank=False, null=False)
-    time = models.TimeField(default=None, blank=True, null=True)
-    place = models.CharField(max_length=512, blank=True, null=True)
-    remarks = models.CharField(max_length=1024, blank=True, null=True)
+    # Creator of the event
+    creator = models.ForeignKey(Locutor)
+    # Maximum number of participants that can register
+    nb_max = models.IntegerField(default=6)
+    # Can anyone attend, or is it a private event?
+    participation = models.IntegerField(default=0)
+    # Can anyone access the subjects/speeches, or is it a restricted diffusion?
+    diffusion = models.IntegerField(default=0)
+
+    # When the eloquence takes place
+    event_date = models.DateField(default=None, blank=False, null=False)
+    event_time = models.TimeField(default=None, blank=True, null=True)
+    # Where the eloquence takes place
+    event_place = models.CharField(max_length=512, blank=True, null=True)
+
+    # Additional information for the participants
+    description = models.TextField(max_length=1024, blank=True, null=True)
+
+    # Registrations are open until N days before the event
+    registration_until = models.IntegerField(default=8)
+    # Subjects will be delivered to participants N days before the event
+    subjects_distribution_date = models.IntegerField(default=7)
+
+    # Can a person be assigned is own subject?
+    collide = models.BooleanField(default=False)
+
 
 class Subject(models.Model):
-    owner = models.ForeignKey(User)
+    # Who is assigned this subject?
+    author = models.ForeignKey(Locutor)
+    # To what event this subject is relative to?
     event = models.ForeignKey(Event)
+    # Content of the subject
     text = models.CharField(default="What? So what? Now what?", max_length=512)
+    # When the subject has been proposed
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class Speech(models.Model):
+    # Subject treated by this speech
+    subject = models.ForeignKey(Subject)
+    # Order of passage
+    order = models.IntegerField(blank=False, null=False)
+    # Text of the speech
+    text = models.TextField(max_length=20000)
+    # Audio recording of the speech
+    audio = models.FileField(blank=True, null=True)
+    # When the speech has been added
+    date = models.DateTimeField(auto_now_add=True)
